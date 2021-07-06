ea3dd796d18c3364f697c665e50f27a6625afd1f
Alex <alexander.young86@gmail.com>
2021-07-05 22:47:17 -0500

    udpate07/05

--- restAPIDemo/src/app/app.module.ts
+++ restAPIDemo/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppRoutingModule } from './modules/app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { HomeComponent } from './pages/home/home.component';
 import { AlbumsComponent } from './pages/albums/albums.component';
 import { PostsComponent } from './pages/posts/posts.component';
 import { AboutComponent } from './pages/about/about.component';
@@ -14,13 +13,19 @@ import { UserComponent } from './components/user/user.component';
 import { PostDisplayComponent } from './components/post-layout/post-layout.component';
 import { PostsService } from './services/posts.service';
 import { UsersService } from './services/users.service';
-import { AlbumsService } from './services/albums.service';
 import { CommentsService } from './services/comments.service';
-import { PhotosService } from './services/photos.service';
 import { AlbumComponent } from './components/album/album.component';
 import { PhotoComponent } from './components/photo/photo.component';
 import { CommentComponent } from './components/comment/comment.component';
 import { PostCreateComponent } from './components/post-creator/post-creator.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   imports: [
@@ -28,7 +33,13 @@ import { PostCreateComponent } from './components/post-creator/post-creator.comp
     FormsModule,
     HttpClientModule,
     AppRoutingModule,
-    HomeComponent
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   declarations: [
     AppComponent,
@@ -47,6 +58,6 @@ import { PostCreateComponent } from './components/post-creator/post-creator.comp
   bootstrap: [
     AppComponent
   ],
-  providers: [PostsService, UsersService, AlbumsService, CommentsService, PhotosService]
+  providers: [PostsService, UsersService, CommentsService]
 })
 export class AppModule { }
--- restAPIDemo/src/app/modules/app-routing.module.ts
+++ restAPIDemo/src/app/modules/app-routing.module.ts
@@ -3,14 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from '../pages/home/home.component';
 import { PostsComponent } from '../pages/posts/posts.component';
-import { AlbumsComponent } from '../pages/albums/albums.component';
-import { AboutComponent } from '../pages/about/about.component';
+import { CommentComponent } from '../components/comment/comment.component';
+
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'posts', component: PostsComponent},
-  {path: 'albums', component: AlbumsComponent},
-  {path: 'about', component: AboutComponent}
+  {path: 'comment', component: CommentComponent},
 ]
 
 @NgModule({
